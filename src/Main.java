b597c438ca3b6353ad25105a62879e80aa9abaa6
Vivek Jain <jain.vivek.anand@gmail.com>
2017-03-22 09:24:15 +0530

    Driver program to run VFAR

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+/**
+ * Driver program to run vertical fragmentation
+ * @author vivek and sourabh
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		new VerticalFragmetation();
+	}
+}
